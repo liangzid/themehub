d98d1095ef3d3a837d52ed8b7bc60bc1a0b17fad
liangzid <2273067585@qq.com>
2024-04-15 21:14:15 +0800

    initial update.

---
+++ colors.lisp
@@ -0,0 +1,14 @@
+
+(setq transformer
+      (list
+     ("blue"  "#C2E8F7"  (194 232 247))
+     ("red"  "#FCE0E1"  (252 224 225))
+     ("yellow"  "#F2F4C1"  (242 244 193))
+     ("orange"  "#FFE2BB"  (255 225 187))
+     ("orange"  "#FFE2BB"  (255 225 187))
+     ("green"  "#CCE7CF"  (204 231 207))
+     ("grey-dark"  "#DBDFEF"  (219 223 239))
+     ("grey-white"  "#F3F3F4"  (243 243 244))
+     ("purple"  "#C5BEDF"  (197 190 223))
+        )
+)
